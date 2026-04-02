3250249ea9f22ef7ad5f97eaaf842e6f56844fad
Jamie <jamie7williams@gmail.com>
2026-04-02 13:59:21 +0100

    feat: create 404 page in 404.php

---
+++ 404.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+
+<main>
+	<h1>404 😦</h1>
+	<p>This page doesn't exist.</p>
+	<a href="<?php echo esc_url( home_url( '/' ) ); ?>">← Return home</a>
+</main>
+
+<?php get_footer(); ?>
