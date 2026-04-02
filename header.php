bd7e761cb0cc77ec5a6eeb4880aa8b38f43fcd8e
Jamie <jamie7williams@gmail.com>
2026-04-02 13:08:18 +0100

    feat: create basic markup for header in header.php

---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+	<header>
+		<div>
+			<img />
+			<h1><?php bloginfo( 'name' ); ?></h1>
+		</div>
+		
+		<h2><?php bloginfo( 'description' ); ?></h2>
+			
+		<nav>
+			
+		</nav>
+	</header>
