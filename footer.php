2c33c164ceca7eb521bfca56c232d0cb2da3ad03
Jamie <jamie7williams@gmail.com>
2026-04-02 13:16:02 +0100

    feat: create markup for footer in footer.php

---
+++ footer.php
@@ -0,0 +1,9 @@
+		<footer>
+			<a>↑ Return to top</a>
+			<p>jamiewilliams.me</p>
+			<p>Website designed and developed by Jamie Williams → jamie7williams@gmail.com</p>
+		</footer>
+
+		<?php wp_footer(); ?>
+	</body>
+</html>
