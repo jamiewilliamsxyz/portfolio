c206bff6db8421623629dadcdee93cabfbb87497
Jamie <jamie7williams@gmail.com>
2026-04-03 13:57:21 +0100

    feat: add education template part

---
+++ template-parts/about-section/education.php
@@ -0,0 +1,15 @@
+<?php $education = jpt_get_about_data( 'education' ); ?>
+
+<div>
+	<h3>Certifications</h3>
+
+	<?php foreach ( $education as $item ) : ?>
+		<div>
+			<a href="<?php echo esc_url( $item['link'] ); ?>">
+				<p><?php echo esc_html( $item['title'] ); ?></p>
+			</a>
+
+			<p><?php echo esc_html( $item['dates'] ); ?></p>
+		</div>
+	<?php endforeach; ?>
+</div>
