42293ecf56073988c387045f7adc8cfd40dbd3ec
Jamie <jamie7williams@gmail.com>
2026-04-03 13:32:28 +0100

    feat: scaffold about section template parts and front-page layout

--- front-page.php
+++ front-page.php
@@ -1,6 +1,21 @@
 <?php get_header(); ?>
 
 <main>
+	<section>
+		<?php get_template_part( 'template-parts/about-section/my-story' ); ?>
+		<div>
+			<?php
+			get_template_part( 'template-parts/about-section/past-work' );
+			get_template_part( 'template-parts/about-section/certifications' );
+			get_template_part( 'template-parts/about-section/education' );
+			get_template_part( 'template-parts/about-section/skills-and-tools' );
+			?>
+		</div>
+	</section>
+
+	<section>
+
+	</section>
 </main>
 
 <?php get_footer(); ?>
