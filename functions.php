82284dc3a8f5f8c6ae08ad6bd146c2ad2d4cd452
Jamie <jamie7williams@gmail.com>
2026-04-03 13:23:23 +0100

    feat: add helper function to get about section data in functions.php

--- functions.php
+++ functions.php
@@ -7,3 +7,14 @@ require_once $template_dir . '/includes/enqueue.php';
 
 // Hide Admin Bar
 add_filter( 'show_admin_bar', '__return_false' );
+
+// Get About Section Data
+function jpt_get_about_data( $key ) {
+	static $data = null;
+
+	if ( null === $data ) {
+		$data = include get_template_directory() . '/includes/about-data.php';
+	}
+
+	return $data[ $key ] ?? null;
+}
