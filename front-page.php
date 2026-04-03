00eba8c85c7238d5c1694ccbbf05f0249b26df71
Jamie <jamie7williams@gmail.com>
2026-04-03 14:01:20 +0100

    feat: add heading to about section in front-page.php

--- front-page.php
+++ front-page.php
@@ -2,14 +2,17 @@
 
 <main>
 	<section>
-		<?php get_template_part( 'template-parts/about-section/my-story' ); ?>
+		<h3>About Me 👨‍💻</h3>
 		<div>
-			<?php
-			get_template_part( 'template-parts/about-section/past-work' );
-			get_template_part( 'template-parts/about-section/certifications' );
-			get_template_part( 'template-parts/about-section/education' );
-			get_template_part( 'template-parts/about-section/skills-and-tools' );
-			?>
+			<?php get_template_part( 'template-parts/about-section/my-story' ); ?>
+			<div>
+				<?php
+				get_template_part( 'template-parts/about-section/past-work' );
+				get_template_part( 'template-parts/about-section/certifications' );
+				get_template_part( 'template-parts/about-section/education' );
+				get_template_part( 'template-parts/about-section/skills-and-tools' );
+				?>
+			</div>
 		</div>
 	</section>
 
