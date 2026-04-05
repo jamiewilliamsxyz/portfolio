5f032b616abc3eb2f0323e0488e40ed9006c1122
Jamie <jamie7williams@gmail.com>
2026-04-05 16:27:19 +0100

    fix: conditionally render header name as h1 on front page only

--- header.php
+++ header.php
@@ -11,12 +11,16 @@
 			<?php if ( has_custom_logo() ) : ?>
 				<div><?php the_custom_logo(); ?></div>
 			<?php endif; ?>
-			
-			<h1><?php bloginfo( 'name' ); ?></h1>
+
+			<?php if ( is_front_page() ) : ?>
+				<h1><?php bloginfo( 'name' ); ?></h1>
+			<?php else : ?>
+				<p><?php bloginfo( 'name' ); ?></p>
+			<?php endif; ?>
 		</div>
-		
+
 		<h2><?php bloginfo( 'description' ); ?></h2>
-			
+
 		<nav>
 			<?php
 			wp_nav_menu(
