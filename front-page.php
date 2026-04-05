5f01d3186f87abc8257e95a591baae28b28ed0c5
Jamie <jamie7williams@gmail.com>
2026-04-05 13:27:37 +0100

    feat: conditionally render projects section based on query results

--- front-page.php
+++ front-page.php
@@ -1,4 +1,13 @@
-<?php get_header(); ?>
+<?php
+get_header();
+
+$projects = new WP_Query(
+	array(
+		'post_type'      => 'jpt_project',
+		'posts_per_page' => -1,
+	)
+);
+?>
 
 <main>
 	<section>
@@ -17,30 +26,23 @@
 		</div>
 	</section>
 
-	<hr />
+	<?php if ( $projects->have_posts() ) : ?>
+		<hr />
 
-	<section>
-		<h3>Projects ✨</h3>
+		<section>
+			<h3>Projects ✨</h3>
 
-		<div>
-			<?php
-			$projects = new WP_Query(
-				array(
-					'post_type'      => 'jpt_project',
-					'posts_per_page' => -1,
-				)
-			);
-
-			if ( $projects->have_posts() ) :
+			<div>
+				<?php
 				while ( $projects->have_posts() ) :
 					$projects->the_post();
 					get_template_part( 'template-parts/project/card' );
 				endwhile;
 				wp_reset_postdata();
-			endif;
-			?>
-		</div>
-	</section>
+				?>
+			</div>
+		</section>
+	<?php endif; ?>
 </main>
 
 <?php get_footer(); ?>
