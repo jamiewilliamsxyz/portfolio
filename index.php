dbbca23aea6cb3dffea326ef1f0ed4908920e00b
Jamie <jamie7williams@gmail.com>
2026-04-08 12:33:33 +0100

    feat: add index.php fallback template

--- index.php
+++ index.php
@@ -1,6 +1,28 @@
-<?php get_header(); ?>
+<?php
+get_header();
+
+$projects = new WP_Query(
+	array(
+		'post_type'      => 'jpt_project',
+		'posts_per_page' => -1,
+	)
+);
+?>
 
 <main>
+	<?php if ( $projects->have_posts() ) : ?>
+		<section>
+			<div>
+				<?php
+				while ( $projects->have_posts() ) :
+					$projects->the_post();
+					get_template_part( 'template-parts/project/card' );
+				endwhile;
+				wp_reset_postdata();
+				?>
+			</div>
+		</section>
+	<?php endif; ?>
 </main>
 
 <?php get_footer(); ?>
