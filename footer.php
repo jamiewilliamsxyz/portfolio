83eb2e2b23a4fd1838ad52a033771c11ce44adb0
Jamie <jamie7williams@gmail.com>
2026-04-09 14:37:38 +0100

    style: add subtext class to footer text; adjust footer link text in footer.php

--- footer.php
+++ footer.php
@@ -1,11 +1,11 @@
 		<hr />
 		<footer>
 			<?php if ( ! is_404() ) : ?>
-				<a href="#top">↑ Return to top</a>
+				<a href="#top">↑ Top</a>
 			<?php endif; ?>
 			
 			<p class="jpt-footer-title">jamiewilliams.me</p>
-			<p>Website designed and developed by Jamie Williams → jamie7williams@gmail.com</p>
+			<p class="jpt-subtext">Website designed and developed by Jamie Williams → jamie7williams@gmail.com</p>
 		</footer>
 
 		<?php wp_footer(); ?>
